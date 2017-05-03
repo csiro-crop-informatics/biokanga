09e16f7debd0b56abd26a40b799a26113b05dc15
Alex Whan <alex.whan@csiro.au>
2017-05-03 16:26:51 +1000

    trying build fix

--- genbioseq/stdafx.h
+++ genbioseq/stdafx.h
@@ -4,10 +4,11 @@
 //
 
 #pragma once
-
+#ifdef WIN32
 #define _CRT_SECURE_NO_DEPRECATE 1
 #define _CRT_NONSTDC_NO_DEPRECATE 1
 #include <iostream>
 #include <tchar.h>
 
 // TODO: reference additional headers your program requires here
+#endif
